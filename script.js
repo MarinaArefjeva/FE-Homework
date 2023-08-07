dee26ecb8690a002d75637fb524c983f650abc63
MarinaArefjeva <130138294+MarinaArefjeva@users.noreply.github.com>
2023-08-07 14:26:02 +0200

    script

---
+++ script.js
@@ -0,0 +1,69 @@
+//1
+// let user = {
+//   name: "John",
+//   age: 30,
+// };
+// console.log(user.age);
+
+//2
+
+//   let product = {
+//     product_name: "iphone14",
+//     product_price: 879,
+//     product_quantity: 10,
+//     product_quality: "new",
+//   };
+//   delete product.quality;
+//   console.log(product);
+
+//5
+// let menu = {
+//   width: 400,
+//   height: 600,
+//   title: "my menu",
+// };
+// //Числа в объекте под ключами 'width''height;
+// console.log(menu.width);
+// console.log(menu.height);
+
+// num_1 = 400;
+// num_2 = 600;
+// console.log(num_1 * 2);
+// console.log(num_2 * 2);
+
+//6
+// function min(a, b) {
+//   if (a > b) {
+//     return b;
+//   }
+//   return a;
+//   if (a < b) {
+//     return a;
+//   }
+//   if (a == b) return a;
+// }
+
+// console.log(min(5, 1));
+// console.log(min(-1, 10));
+// console.log(min(0, 0));
+
+//7
+// function foo(a, b) {
+//   if (a % 2 && b % 2) {
+//     // Если оба числа нечётные
+//     return a + b;
+//   } else if (a % 2 || b % 2) {
+//     // Если одно из них чётное
+//     if (a % 2) {
+//       // Если a нечётное
+//       return a;
+//     }
+//     return b; // Если а нечётное, скрипт до сюда не дойдёт
+//   } else {
+//     // Если оба чётные
+//     return a * b;
+//   }
+//}
+// console.log(foo(7, 5));
+// console.log(foo(2, 7));
+// console.log(foo(6, 10));
